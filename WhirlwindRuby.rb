36a25712746a20c73b38c06b73fd77da9cc722c5
Delissa <delissacbeaman@gmail.com>
2020-06-01 00:12:08 -0500

    practice problems

---
+++ WhirlwindRuby.rb
@@ -0,0 +1,7 @@
+# Create a program that asks the user for their favorite 5 foods. Then display those foods as an array, using the “p” keyword.
+
+puts "Hello, What are your five favorite foods?"
+i = 0
+While i < 5
+puts gets.chomp 
+end
