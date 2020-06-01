1bf6755336df05ce6dc5ef6b6ea1692703304def
Delissa <delissacbeaman@gmail.com>
2020-06-01 15:53:27 -0500

    complete up to number 6

--- arrays_reduce_practice_problems.rb
+++ arrays_reduce_practice_problems.rb
@@ -1,32 +1,52 @@
-#  1. Start with an array of numbers and compute the sum of all the numbers.
-numbers = [5, 10, 8, 3]
-sum = 0
+# #  1. Start with an array of numbers and compute the sum of all the numbers.
+# numbers = [5, 10, 8, 3]
+# sum = 0
 
-numbers.each do |number|
-  sum = sum + number
-end
-p sum
+# numbers.each do |number|
+#   sum = sum + number
+# end
+# p sum
 
-#  2. Start with an array of strings and combine them all into a single string.
-words = ["volleyball", "basketball", "badminton"]
-new = ""
+# #  2. Start with an array of strings and combine them all into a single string.
+# words = ["volleyball", "basketball", "badminton"]
+# new = ""
 
-words.each do |word|
-  new = new + word
-end
-p new
+# words.each do |word|
+#   new = new + word
+# end
+# p new
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
 
-#  4. Start with an array of numbers and compute the the minumum number.
-#     For example, [5, 10, 8, 3, 9] becomes 3.
+# #  4. Start with an array of numbers and compute the the minumum number.
+# numbers = [5, 10, 8, 3, 9]
+# minimum = numbers[0]
+# numbers.each do |number|
+#   if number < minimum
+#     minimum = number
+#   end
+# end
+# p minimum
 
-#  5. Start with an array of strings and compute the total length of all the strings.
-#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+# # 5.Start with an array of strings and compute the total length of all the strings.
+# words = ["book", "look", "took"]
+# length = 0
+
+# words.each do |word|
+#   length += word.length
+# end
+# p length
 
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
-#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+products = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+
+cheapest_product = products[0]
+products.each do |product|
+  if product[:price] < cheapest_product[:price]
+  end
+end
+p cheapest_product
 
 #  7. Start with an array of numbers and compute product of all the numbers.
 #     For example, [5, 10, 8, 3] becomes 1200.
