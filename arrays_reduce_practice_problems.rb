55512f82e81c03339d4fdbe196815071888d1e3f
Delissa <delissacbeaman@gmail.com>
2020-06-01 00:39:27 -0500

    map practice problem #2

--- arrays_reduce_practice_problems.rb
+++ arrays_reduce_practice_problems.rb
@@ -8,7 +8,13 @@ end
 p sum
 
 #  2. Start with an array of strings and combine them all into a single string.
-#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+words = ["volleyball", "basketball", "badminton"]
+new = ""
+
+words.each do |word|
+  new = new + word
+end
+p new
 
 #  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
