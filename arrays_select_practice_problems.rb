95e3dad3daf0b2b958ea54ca733ab3cfe38aa5eb
Delissa <delissacbeaman@gmail.com>
2020-06-23 22:59:14 -0500

    question 5

--- arrays_select_practice_problems.rb
+++ arrays_select_practice_problems.rb
@@ -37,17 +37,26 @@
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-numbers = [2, 4, 5, 1, 8, 9, 7]
-new = []
-numbers.each do |number|
-  if number % 2 == 0
-    new << number
-  end
-end
-p new
+# numbers = [2, 4, 5, 1, 8, 9, 7]
+# new = []
+# numbers.each do |number|
+#   if number % 2 == 0
+#     new << number
+#   end
+# end
+# p new
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+new = []
+strings.each do |string|
+  if string.length < 4
+    new << string
+  end
+end
+p new
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
