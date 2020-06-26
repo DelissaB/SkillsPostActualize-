3a5a4903bd96c079aac79b65a3bfcfbd61fe2a6d
Delissa <delissacbeaman@gmail.com>
2020-06-25 22:49:26 -0500

    number 10

--- arrays_select_practice_problems.rb
+++ arrays_select_practice_problems.rb
@@ -101,15 +101,22 @@
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-hashes = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
+# hashes = [{ name: "chair", price: 100 }, { name: "pencil", price: 1 }, { name: "book", price: 4 }]
 
-new_hashes = hashes.select do |hash|
-  hash[:price] < 10
-end
-p new_hashes
+# new_hashes = hashes.select do |hash|
+#   hash[:price] < 10
+# end
+# p new_hashes
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+
+new_numbers = numbers.select do |number|
+  number % 2 == 1
+end
+p new_numbers
 
 # SOLUTIONS (using while loop): https://gist.github.com/peterxjang/7de16ed43ea506e98df3fa15074b84f8
 # SOLUTIONS (using .each shortcut): https://gist.github.com/peterxjang/a702894841c7018ed8c127b647ae21f8
