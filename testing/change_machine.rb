8ff86d196a9595f47458ba4f55001074428bc822
Delissa <delissacbeaman@gmail.com>
2020-06-28 16:57:34 -0500

    Test for nickel passes

--- testing/change_machine.rb
+++ testing/change_machine.rb
@@ -3,7 +3,16 @@ require "rspec"
 
 class ChangeMachine
   def change(cents)
-    [1]
+    coins = []
+    while cents >= 5
+      coins << 5
+      cents -= 5
+    end
+    while cents > 0
+      coins << 1
+      cents -= 1
+    end
+    coins
   end
 end
 
@@ -13,6 +22,21 @@ RSpec.describe ChangeMachine do
       change_machine = ChangeMachine.new
       result = change_machine.change(1)
       expect(result).to eq([1])
+    end
+    it "should return [1,1] when given 2" do
+      change_machine = ChangeMachine.new
+      result = change_machine.change(2)
+      expect(result).to eq([1, 1])
+    end
+    it "should return [1,1,1] when given 2" do
+      change_machine = ChangeMachine.new
+      result = change_machine.change(3)
+      expect(result).to eq([1, 1, 1])
+    end
+    it "should return [5] when given 5" do
+      change_machine = ChangeMachine.new
+      result = change_machine.change(5)
+      expect(result).to eq([5])
     end
   end
 end
