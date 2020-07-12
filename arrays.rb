8513da26ec4ddfda5f80a58f816d418aa94e9b26
Delissa <delissacbeaman@gmail.com>
2020-07-11 23:42:10 -0500

    arrays 3

--- arrays.rb
+++ arrays.rb
@@ -5,12 +5,16 @@
 # p words
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-words = ["a", "b", "c", "d"]
-words[1] = 2
-p words
+# words = ["a", "b", "c", "d"]
+# words[1] = 2
+# p words
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+numbers = [1, 2, 3, 4, 5]
+numbers.each do |number|
+  p number
+end
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
