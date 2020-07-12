8c8a33e385fba28ca51af1e378813593dab2be6c
Delissa <delissacbeaman@gmail.com>
2020-07-12 12:01:49 -0500

    converting pratice 4

--- converting_data_practice_problems.rb
+++ converting_data_practice_problems.rb
@@ -15,15 +15,15 @@
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-colors = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
-new_colors = {}
-i = 0
+# colors = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+# new_colors = {}
+# i = 0
 
-while i < colors.length
-  new_colors[colors[i][:id]] = colors[i]
-  i += 1
-end
-p new_colors
+# while i < colors.length
+#   new_colors[colors[i][:id]] = colors[i]
+#   i += 1
+# end
+# p new_colors
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
@@ -43,6 +43,14 @@ p new_colors
 
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+things = { "chair" => 100, "book" => 14 }
+name_price_pairs = []
+
+things.each do |name, price|
+  name_price_pairs << [name, price]
+end
+p name_price_pairs
 
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
