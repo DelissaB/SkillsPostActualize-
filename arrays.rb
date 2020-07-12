68265ea35e713a9ed5624ba7c70272b09e9cfc7b
Delissa <delissacbeaman@gmail.com>
2020-07-12 00:00:08 -0500

    arrays 8

--- arrays.rb
+++ arrays.rb
@@ -38,11 +38,14 @@
 # end
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
-words = ["hello", "bye"]
-words << "baby"
-p words
+# words = ["hello", "bye"]
+# words << "baby"
+# p words
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+numbers = [1, 2, 3, 4, 5]
+numbers[0] = numbers[0] * 10
+p numbers
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
