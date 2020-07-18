162d60defa473884c1ec173f410c1db86ac34baf
DelissaB <delissacbeaman@gmail.com>
2020-07-17 22:04:31 -0500

    Arrays map 2

--- arrays_map_practice.rb
+++ arrays_map_practice.rb
@@ -1,17 +1,26 @@
 # 1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
-numbers = [1, 2, 3]
-new_numbers = []
-i = 0
-while i < numbers.length
-  new_numbers << numbers[i] * 3
-  i+=1
-end
-p new_numbers
+# numbers = [1, 2, 3]
+# new_numbers = []
+# i = 0
+# while i < numbers.length
+#   new_numbers << numbers[i] * 3
+#   i+=1
+# end
+# p new_numbers
 
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+words = ["hello", "goodbye"]
+new_words = []
+words.each do |word|
+  new_words << word.upcase
+end
+p new_words
+
+
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
