2d1d98aa072850e552e26df0cb87cab4a365683d
DelissaB <delissacbeaman@gmail.com>
2020-07-18 21:58:47 -0500

    arrays 2

--- arrays_solved_as_select.rb
+++ arrays_solved_as_select.rb
@@ -1,14 +1,22 @@
 # 1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-numbers = [2, 32, 80, 18, 12, 3]
-new_numbers = numbers.select do |number|
-  number < 20
-end
-p new_numbers
+# numbers = [2, 32, 80, 18, 12, 3]
+# new_numbers = numbers.select do |number|
+#   number < 20
+# end
+# p new_numbers
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+words = ["winner", "winner", "chicken", "dinner"]
+new_words = words.select do |word|
+  if word[0] == "w"
+    new_words = word
+  end
+end
+p new_words
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
