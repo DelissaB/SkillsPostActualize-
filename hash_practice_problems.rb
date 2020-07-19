738b7df30696d9ef39e34fc303d92849dbe41ad0
DelissaB <delissacbeaman@gmail.com>
2020-07-19 09:51:07 -0500

    hash practice complete

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -1,64 +1,64 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
-# person = {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}
+person = {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}
 
-# p person["first name"]
-# p person["last name"]
-# p person["email"]
+p person["first name"]
+p person["last name"]
+p person["email"]
 
 # # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
-# person = [{"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}, {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}, {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}]
-#  p person[0]["first name"]
-#  p person[0]["last name"]
+person = [{"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}, {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}, {"first name" => "Delissa", "last name" => "Beaman", "email" => "delissa@yahoo.com"}]
+ p person[0]["first name"]
+ p person[0]["last name"]
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
-# items = {"book" => 5, "toothpaste" => 3, "shoestring" => 1}
-#  items["car"] = 400
-# p items
+items = {"book" => 5, "toothpaste" => 3, "shoestring" => 1}
+ items["car"] = 400
+p items
 
 # 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
 
-# book = { "title" => "Medical Apartheid", "author" => "Someone beautiful and black", "number of pages" => 200, "language" => "English"}
-# p book["title"]
-# p book["author"]
-# p book["number of pages"]
-# p book["language"]
+book = { "title" => "Medical Apartheid", "author" => "Someone beautiful and black", "number of pages" => 200, "language" => "English"}
+p book["title"]
+p book["author"]
+p book["number of pages"]
+p book["language"]
 
 
 # 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
-# books = { "title" => "Medical Apartheid", "author" => "Someone beautiful and black king"}, { "title" => "Becoming", "author" => "Someone beautiful and black queen"}, { "title" => "Happiness", "author" => "Someone beautiful and black queen"}
+books = { "title" => "Medical Apartheid", "author" => "Someone beautiful and black king"}, { "title" => "Becoming", "author" => "Someone beautiful and black queen"}, { "title" => "Happiness", "author" => "Someone beautiful and black queen"}
 
-# p books[0]["author"]
+p books[0]["author"]
 
 
 # 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
 
-# places = {"Illinois" => "Springfield", "Arizona" => "Phoenix","Alaska" => "Juneau"}
-# places["Alabama"] = "Montgomery"
-# p places
+places = {"Illinois" => "Springfield", "Arizona" => "Phoenix","Alaska" => "Juneau"}
+places["Alabama"] = "Montgomery"
+p places
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-# laptop = {"Brand" => "Macbook", "model" => "Air", "year" => 2015}
+laptop = {"Brand" => "Macbook", "model" => "Air", "year" => 2015}
 
-# p laptop["Brand"]
-# p laptop["model"]
-# p laptop["year"]
+p laptop["Brand"]
+p laptop["model"]
+p laptop["year"]
 
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
 
-# laptop = [{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"}]
+laptop = [{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"}]
 
-# p laptop[1]["model"]
+p laptop[1]["model"]
 
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
-# dictionary = {"book" => "something to read", "fruit" => "Delicious"}
-# p dictionary["book"]
+dictionary = {"book" => "something to read", "fruit" => "Delicious"}
+p dictionary["book"]
 
 # 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
 
