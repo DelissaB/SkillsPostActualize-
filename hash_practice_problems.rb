8361a161e621025bd933fce42277541cd9a5b5b4
DelissaB <delissacbeaman@gmail.com>
2020-07-19 09:45:05 -0500

    hash practice 8

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -41,14 +41,19 @@
 
 # 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
-laptop = {"Brand" => "Macbook", "model" => "Air", "year" => 2015}
+# laptop = {"Brand" => "Macbook", "model" => "Air", "year" => 2015}
 
-p laptop["Brand"]
-p laptop["model"]
-p laptop["year"]
+# p laptop["Brand"]
+# p laptop["model"]
+# p laptop["year"]
 
 
 # 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+laptop = [{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"},{"Brand" => "Macbook", "model" => "Air"}]
+
+p laptop[1]["model"]
+
 
 # 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
