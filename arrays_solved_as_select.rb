1e3289e73050dec4231177d37e71cd0a02a101b7
DelissaB <delissacbeaman@gmail.com>
2020-07-18 22:08:30 -0500

    arrays 3

--- arrays_solved_as_select.rb
+++ arrays_solved_as_select.rb
@@ -10,16 +10,24 @@
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
 #     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-words = ["winner", "winner", "chicken", "dinner"]
-new_words = words.select do |word|
-  if word[0] == "w"
-    new_words = word
-  end
-end
-p new_words
+# words = ["winner", "winner", "chicken", "dinner"]
+# new_words = words.select do |word|
+#   if word[0] == "w"
+#     new_words = word
+#   end
+# end
+# p new_words
 
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+small_items = items.select do |item|
+  if item[:price] < 5
+    small_items = item
+  end
+end
+p small_items
+
 
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
