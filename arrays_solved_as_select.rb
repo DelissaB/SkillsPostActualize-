8a2172430ce72c9c7fd2b8621ef83d1d82651419
DelissaB <delissacbeaman@gmail.com>
2020-07-18 23:36:50 -0500

    arrays 10

--- arrays_solved_as_select.rb
+++ arrays_solved_as_select.rb
@@ -90,14 +90,22 @@
 #  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
-new_items = items.select do |item|
-  if item[:price] < 10
-    new_items = item
-  end
-end
-p new_items
+# items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+# new_items = items.select do |item|
+#   if item[:price] < 10
+#     new_items = item
+#   end
+# end
+# p new_items
 
 
 # 10. Start with an array of numbers and create a new array with only the odd numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+new_numbers = numbers.select do |number|
+  if number % 2 == 1
+    new_numbers = number
+  end
+end
+p new_numbers
