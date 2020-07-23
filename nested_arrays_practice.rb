bccf10ae2e6530cc71e4661af22562047d072aaa
DelissaB <delissacbeaman@gmail.com>
2020-07-22 20:37:58 -0500

    nested loops 4

--- nested_arrays_practice.rb
+++ nested_arrays_practice.rb
@@ -38,23 +38,67 @@
 #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
-letters = ["a", "b", "c", "d"]
-letter_combinations = []
-index1 = 0
-while index1 < letters.length
-  index2 = 0
-  while index2 < letters.length
-    if index1 != index2
-      letter_combinations << letters[index1] + letters[index2]
-    end
-    index2 += 1
-  end
-  index1 += 1
-end
-p letter_combinations
+# letters = ["a", "b", "c", "d"]
+# letter_combinations = []
+# index1 = 0
+# while index1 < letters.length
+#   index2 = 0
+#   while index2 < letters.length
+#     if index1 != index2
+#       letter_combinations << letters[index1] + letters[index2]
+#     end
+#     index2 += 1
+#   end
+#   index1 += 1
+# end
+# p letter_combinations
 
 #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
 #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+
+    #  numbers = [5, -2, 1, -9, -7, 2, 6]
+    #  index1 = 0
+    #  max_product = numbers[0] * numbers[1]
+     
+    #  while index1 < numbers.length
+    #     current_number = numbers[index1]
+    #     index2 = 0
+    #     while index2 < new_numbers.length
+    #         if index1 != index2
+    #           other_number = numbers[index2]
+    #           product = current_number * other_number
+    #           if product > max_product
+    #             max_product = product
+    #         end
+    #     end
+    #     index2 = index2 + 1
+    #   end
+    #   index1 = index1 + 1 
+    #   end
+    #   p max_product
+
+
+numbers = [5, -2, 1, -9, -7, 2, 6]
+max_product = numbers[0] * numbers[1]
+index1 = 0
+while index1 < numbers.length
+  current_number = numbers[index1]
+  index2 = 0
+  while index2 < numbers.length
+    if index1 != index2
+      other_number = numbers[index2]
+      product = current_number * other_number
+      if product > max_product
+        max_product = product
+      end
+    end
+    index2 = index2 + 1
+  end
+  index1 = index1 + 1
+end
+p max_product
+
+
 
 #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
